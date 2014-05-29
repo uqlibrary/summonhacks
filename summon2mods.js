f34f2824df50364e425ab522781f9b8b807af9a6
Christiaan Kortekaas <ck@uq.edu.au>
2014-05-29 13:09:09 +1000

    Fixed css url (removed http://)

--- summon2mods.js
+++ summon2mods.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var cssPath = '//http://raw.githubusercontent.com/uqlibrary/summonhacks/master/',libDetailPageId, newHref, libCurrentURL, record, recordParts, thisID,libCurrentURL = window.location.hash.substring(1);
+  var cssPath = '//raw.githubusercontent.com/uqlibrary/summonhacks/master/',libDetailPageId, newHref, libCurrentURL, record, recordParts, thisID,libCurrentURL = window.location.hash.substring(1);
   // Add custom styles
   $('head').append('<link rel="stylesheet" type="text/css" href="' + cssPath + 'summon2mods.css" />');
 
