439da3189009efa859aded8f55020fecda18fc14
Andrew Martlew <a.martlew@library.uq.edu.au>
2014-09-15 09:56:25 +1000

    Removed CSS overrides in JS / Catch errors

--- summon2mods.js
+++ summon2mods.js
@@ -1,25 +1,15 @@
 // Thanks to GVSU https://github.com/gvsulib/Summon-2.0-Scripts) and others for sharing your summon hacks
 
 $(document).ready(function() {
-
-    //Dev
-    //  var cssPath = '//bypass.hazchem.library.uq.edu.au/summonhacks/summonhacks/',libDetailPageId, newHref, libCurrentURL, record, recordParts, thisID,libCurrentURL = window.location.hash.substring(1);
-    //Staging
     var cssPath = '//www.library.uq.edu.au/summonhacks/',
         libDetailPageId, newHref, libCurrentURL, record, recordParts, thisID = window.location.hash.substring(1);
-    // Add custom styles
-    $('head').append('<link rel="stylesheet" type="text/css" href="' + cssPath + 'summon2mods.css" />');
-
-    // Accessibility added for advanced search titles
-    $('div.searchBox div.queryBox span.caret').parent().attr('title', 'Advanced Search');
-    $('div.search div.queryBox span.caret').parent().attr('title', 'Advanced Search');
-
-    // Adds higher resolution logo image:
-    // for mobile
-    document.getElementsByClassName("col Logo ng-isolate-scope")[0].getElementsByTagName('img')[0].src = 'https://www.library.uq.edu.au/summonhacks/LibraryWhite.png';
-    document.getElementsByClassName("col Logo ng-isolate-scope")[0].getElementsByTagName('img')[0].title = 'UQ Library Homepage';
-    // for tablets and larger
-    document.getElementsByClassName("col hidden-phone Logo ng-isolate-scope")[0].getElementsByTagName('img')[0].src = 'https://www.library.uq.edu.au/summonhacks/LibraryWhite.png';
-    // document.getElementsByClassName("col hidden-phone Logo ng-isolate-scope")[0].getElementsByTagName('img')[0].alt = 'UQ Library Homepage';
-    document.getElementsByClassName("col hidden-phone Logo ng-isolate-scope")[0].getElementsByTagName('img')[0].title = 'UQ Library Homepage';
+    try {
+      // Add custom styles
+      $('head').append( '<link rel="stylesheet" type="text/css" href="' + cssPath + 'summon2mods.css" />');
+      // Accessibility added for advanced search titles
+      $('div.searchBox div.queryBox span.caret').parent().attr('title', 'Advanced Search');
+      $('div.search div.queryBox span.caret').parent().attr('title', 'Advanced Search');
+      // Adds a title to the logo img
+      $( ".customHeader .Logo img" ).attr( "title", "UQ Library Homepage" );
+    } catch (e) {}
 });
