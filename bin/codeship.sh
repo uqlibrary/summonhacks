5799a9738008ff124234eab428b66408de19878f
Dan Callan <d.callan@library.uq.edu.au>
2015-01-12 14:55:26 +1000

    added s3 folder sed to codeship.sh

--- bin/codeship.sh
+++ bin/codeship.sh
@@ -12,6 +12,7 @@ awsconfig="aws.json"
 sed -i -e "s#<AWSAccessKeyId>#${AWSAccessKeyId}#g" ${awsconfig}
 sed -i -e "s#<AWSSecretKey>#${AWSSecretKey}#g" ${awsconfig}
 sed -i -e "s#<S3Bucket>#${S3Bucket}#g" ${awsconfig}
+sed -i -e "s#<S3Folder>#${S3Folder}#g" ${awsconfig}
 sed -i -e "s#<CFDistribution>#${CFDistribution}#g" ${awsconfig}
 sed -i -e "s#<AWSRegion>#${AWSRegion}#g" ${awsconfig}
 
