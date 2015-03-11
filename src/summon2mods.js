d3f8054252ac6e6c3d9aa5448360dfb70c1c051f
Kit Brown-Watts <kbwatts@gmail.com>
2015-03-12 09:03:23 +1000

    Should fix the value assignment issue

--- src/summon2mods.js
+++ src/summon2mods.js
@@ -9,14 +9,13 @@
       // Add custom styles
       $('head').append('<link rel="stylesheet" type="text/css" href="' + base + '/summon2mods.css" />');
     } catch (e) {}
-    window.__wtw_lucky_site_id = 34528;
+    
+  });
+})();
 
-  (function() {
+  window.__wtw_lucky_site_id = 34528;
+    $(function() {
     var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
     wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
     })();
-  });
-})();
-
-
