07a0cca4296b44d7bacf52bdd093f1c16fb7f60c
Kit Brown-Watts <kbwatts@gmail.com>
2015-03-12 09:36:44 +1000

    back to original

--- src/summon2mods.js
+++ src/summon2mods.js
@@ -1,11 +1,23 @@
-var base = 'https://d3nm82zk9ronst.cloudfront.net/summonhacks';
-// Add custom styles
-$('head').append('<link rel="stylesheet" type="text/css" href="' + base + '/summon2mods.css" />');
+'use strict';
+
+(function () {
+  // Thanks to GVSU https://github.com/gvsulib/Summon-2.0-Scripts) and others for sharing your summon hacks
+  if (typeof jQuery === 'undefined') {
+    return;
+  }
+  $(function () {
+    try {
+      var base = 'https://d3nm82zk9ronst.cloudfront.net/summonhacks';
+      // Add custom styles
+      $('head').append('<link rel="stylesheet" type="text/css" href="' + base + '/summon2mods.css" />');
+    } catch (e) {}
+  });
+})();
 
 window.__wtw_lucky_site_id = 34528;
 
-(function() {
-var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
-wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
-})();
+  (function() {
+    var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+    wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+    })();
