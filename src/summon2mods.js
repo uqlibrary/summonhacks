021d0454bb71dbf7bce7ae4a6c14a9ccb373f7e7
Kit Brown-Watts <kbwatts@gmail.com>
2015-03-12 08:05:29 +1000

    Adds heat mapping to Summon

--- src/summon2mods.js
+++ src/summon2mods.js
@@ -13,3 +13,14 @@
     } catch (e) {}
   });
 })();
+
+
+<script type='text/javascript'>
+window.__wtw_lucky_site_id = 34528;
+
+  (function() {
+    var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+    wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+    })();
+  </script>
