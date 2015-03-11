71c9a8cab7ff62f552157c3c002bfd36abe330a1
Kit Brown-Watts <kbwatts@gmail.com>
2015-03-12 08:56:24 +1000

    Testing 'use strict'

--- src/summon2mods.js
+++ src/summon2mods.js
@@ -8,13 +8,8 @@
   $(function () {
     try {
       var base = 'https://d3nm82zk9ronst.cloudfront.net/summonhacks';
-      window.__wtw_lucky_site_id = 34528;
-      var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
-      wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
       // Add custom styles
       $('head').append('<link rel="stylesheet" type="text/css" href="' + base + '/summon2mods.css" />');
     } catch (e) {}
-    
   });
 })();
