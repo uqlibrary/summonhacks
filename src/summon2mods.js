ba35f51eea787e307cf2b698ef79c197d4a63d2f
Kit Brown-Watts <kbwatts@gmail.com>
2015-03-13 12:09:57 +1000

    Removes heat mapping as we've reached our free quota

--- src/summon2mods.js
+++ src/summon2mods.js
@@ -13,11 +13,3 @@
     } catch (e) {}
   });
 })();
-
-window.__wtw_lucky_site_id = 34528;
-
-  (function() {
-    var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
-    wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
-    })();
