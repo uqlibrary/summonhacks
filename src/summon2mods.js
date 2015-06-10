6f98b2131bf44e6c6aca13f22922dc8f5e4d6c78
Christiaan Kortekaas <ck@uq.edu.au>
2015-06-10 19:24:53 +1000

    Added a temporary warning message about the journal articles indexed as books problem

--- src/summon2mods.js
+++ src/summon2mods.js
@@ -10,6 +10,18 @@
       var base = 'https://d3nm82zk9ronst.cloudfront.net/summonhacks';
       // Add custom styles
       $('head').append('<link rel="stylesheet" type="text/css" href="' + base + '/summon2mods.css" />');
+
+      $('#results').prepend(
+        '<div style="margin: 1em 5% 0 5%;">' +
+        '<p style="font-size: 16px; font-weight: bold; color: red; text-align: left;">' +
+
+        'UQ Library Search is currently experiencing a problem where journal articles appear as books and links do not resolve to the full text article. ' +
+        'This problem will be resolved by Thursday afternoon (June 11th) as we work with our vendor. We apologise for the inconvenience.' +
+
+        '</p>' +
+        '</div>'
+      );
+
     } catch (e) {}
   });
 })();
