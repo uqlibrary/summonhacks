cb8134c5ace0385604491400e0e69d785d41807e
Christiaan Kortekaas <ck@uq.edu.au>
2015-06-12 13:41:11 +1000

    Removed client warning message now that the summon index issues are resolved

--- src/summon2mods.js
+++ src/summon2mods.js
@@ -13,19 +13,3 @@
     } catch (e) {}
   });
 })();
-
-$(document).ready(function() {
-  setTimeout(function() {
-    $('#results').prepend(
-      '<div style="margin: 1em 5% 0 5%;  box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14), 0 1px 8px 0 rgba(0, 0, 0, 0.12), 0 3px 3px -2px rgba(0, 0, 0, 0.4);padding: 4px 16px; background-color: #F5F5F5; border-radius: 2px;">' +
-      '<p style="font-size: 16px; font-weight: bold; color: black; text-align: left;">' +
-
-      'We are experiencing problems with some links to full text. If your search is affected ' +
-      '<a href="https://www.library.uq.edu.au/blog/2015/06/10/problems-links-to-full-text-uq-library-search">' +
-      'please see more information and workarounds' +
-      '</a>' +
-      '</p>' +
-      '</div>'
-    );
-  }, 4000);
-});
